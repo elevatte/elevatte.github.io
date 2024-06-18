9cde9e2085147515ebd3bc2afecc9e3405467bad
Rafael Ricci <rlopes.ricci@hotmail.com>
2024-06-18 17:00:56 -0400

    add script to deploy to gh-pages

---
+++ deply-gh-pages.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Para o script se ocorrer qualquer erro
+set -e
+
+# Gera o build do projeto
+yarn build
+
+# Navega para a pasta build
+cd build
+
+# Inicializa um repositório Git na pasta build
+git init
+
+# Define a branch inicial como gh-pages
+git checkout -b gh-pages
+
+# Adiciona o repositório remoto
+git remote add origin https://github.com/elevatte/elevatte.github.io.git
+
+# Adiciona os arquivos e faz o commit
+git add .
+git commit -m "Deploy to gh-pages"
+
+# Força o push para a branch gh-pages
+git push --force origin gh-pages
+
+# Volta para a pasta raiz do projeto
+cd ..
